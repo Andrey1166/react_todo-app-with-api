cde9ac7b3c1514f5d88b231e77490e0d168de46d
Andrey1166 <aannddrreeyy266@gmail.com>
2026-01-21 23:57:23 +0200

    solution

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,120 @@
+import classNames from 'classnames';
+import { Todo } from '../types/Todo';
+import { useRef, useState } from 'react';
+
+type Props = {
+  todo: Todo;
+  handleTodoDelete: (id: number) => void;
+  processings: Set<number>;
+  handleTodoUpdate: (todo: Todo) => void;
+  handleTodoToggle: (todo: Todo) => void;
+};
+
+export const TodoItem: React.FC<Props> = ({
+  todo,
+  handleTodoDelete,
+  processings,
+  handleTodoUpdate,
+  handleTodoToggle,
+}) => {
+  const [isChecked, setIsChecked] = useState(todo.completed);
+  const [isEdited, setIsEdited] = useState(false);
+  const [editValue, setEditvalue] = useState('');
+  const editedInputField = useRef<HTMLInputElement>(null);
+
+  const isProcessed = processings.has(todo.id);
+  const titleEdit = () => {
+    if (editValue === todo.title) {
+      return;
+    }
+
+    if (editValue.trim() === '') {
+      handleTodoDelete(todo.id);
+    } else {
+      handleTodoUpdate({
+        id: todo.id,
+        title: editValue,
+        userId: todo.userId,
+        completed: todo.completed,
+      });
+    }
+
+    setIsEdited(false);
+  };
+
+  return (
+    <div
+      data-cy="Todo"
+      className={classNames('todo item-enter-done', {
+        completed: todo.completed,
+      })}
+    >
+      <label className="todo__status-label" aria-label="label">
+        <input
+          data-cy="TodoStatus"
+          type="checkbox"
+          className="todo__status"
+          onChange={() => {
+            handleTodoToggle(todo);
+            setIsChecked(!todo.completed);
+          }}
+          checked={isChecked}
+        />
+      </label>
+
+      {!isEdited ? (
+        <>
+          <span
+            data-cy="TodoTitle"
+            className="todo__title"
+            onDoubleClick={() => {
+              setIsEdited(true);
+              setEditvalue(todo.title);
+              setTimeout(() => editedInputField.current?.focus(), 0);
+            }}
+          >
+            {todo.title}
+          </span>
+          <button
+            type="button"
+            className="todo__remove"
+            data-cy="TodoDelete"
+            onClick={() => {
+              handleTodoDelete(todo.id);
+            }}
+          >
+            ×
+          </button>
+        </>
+      ) : (
+        <form
+          onSubmit={event => {
+            event.preventDefault();
+            editedInputField.current?.blur();
+          }}
+        >
+          <input
+            data-cy="TodoTitleField"
+            type="text"
+            className="todo__title-field"
+            placeholder="Empty todo will be deleted"
+            ref={editedInputField}
+            value={editValue}
+            onChange={event => setEditvalue(event.target.value)}
+            onBlur={titleEdit}
+          />
+        </form>
+      )}
+
+      <div
+        data-cy="TodoLoader"
+        className={classNames('modal overlay', {
+          'is-active': isProcessed,
+        })}
+      >
+        <div className="modal-background has-background-white-ter" />
+        <div className="loader" />
+      </div>
+    </div>
+  );
+};
